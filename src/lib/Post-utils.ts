853d0d0dbc3ab1429f74c10e78a62a29e06b2944
thomas abadie <thomas.abadiepro@gmail.com>
2024-12-04 17:47:03 +0100

    changement pour l'affichage des erreurs

--- src/lib/Post-utils.ts
+++ src/lib/Post-utils.ts
@@ -22,7 +22,7 @@ export const getPosts = async () => {
     const files = await fs.readdir(postsDirectory);
     const fileNames = files.filter(f => f.endsWith(".mdx")); // Filtre uniquement les fichiers .mdx
 
-    const posts = [];
+    const posts: Post[] = [];
 
     for await (const fileName of fileNames) {
         const fullPath = path.join(postsDirectory, fileName);
@@ -32,16 +32,19 @@ export const getPosts = async () => {
         const safeData = PostSchema.safeParse(frontMatter.data);
 
         if (!safeData.success) {
-            console.error(`error parsing file: ${fileName} - ${safeData.error.issues.map((i) => i.message).join(", ")}`);
-            continue
+            console.error(`error parsing file: ${fileName}`);
+            safeData.error.issues.forEach((issue) => {
+                console.log(` - ${issue.path.join(" -> ")}: ${issue.message}`);
+            });
+            continue;
         }
-
         posts.push({
             ...safeData.data,
             slug: fileName.replace(".mdx", ""),
             content: frontMatter.content,
         })
     }
+
 
     return posts;
 };
